7536d1100522dd3ec028dcc87d2869cdbe14228e
Tzelalis <ptzelalis@tringlesoft.com>
2024-03-13 05:16:03 +0200

    Add paging depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -131,6 +131,9 @@ dependencies {
     ksp(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
 
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
+
     // Testing and debug
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
