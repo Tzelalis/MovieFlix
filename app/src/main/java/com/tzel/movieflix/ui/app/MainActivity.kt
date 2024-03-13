48fcf86777c88deb9c47baf25a1154eafe4aff5c
Tzelalis <ptzelalis@tringlesoft.com>
2024-03-13 02:50:21 +0200

    Remove status bar padding

--- app/src/main/java/com/tzel/movieflix/ui/app/MainActivity.kt
+++ app/src/main/java/com/tzel/movieflix/ui/app/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.tzel.movieflix.ui.app
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.tzel.movieflix.ui.app.composable.AppScreen
 import com.tzel.movieflix.ui.theme.MovieFlixTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +19,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        remoteStatusBarPadding()
+        window.decorView.setBackgroundColor(resources.getColor(android.R.color.black, null))
+
         setContent {
             MovieFlixTheme {
                 Surface(
@@ -24,6 +32,16 @@ class MainActivity : ComponentActivity() {
                     AppScreen()
                 }
             }
+        }
+    }
+
+    private fun remoteStatusBarPadding() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
         }
     }
 }
