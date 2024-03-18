3ee17230d7c18c734b7fa29e7e7987940139fb95
Tzelalis <ptzelalis@tringlesoft.com>
2024-03-18 05:02:52 +0200

    Add splash delay

--- app/src/main/java/com/tzel/movieflix/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/tzel/movieflix/ui/splash/SplashViewModel.kt
@@ -3,6 +3,7 @@ package com.tzel.movieflix.ui.splash
 import com.tzel.movieflix.ui.core.BaseViewModel
 import com.tzel.movieflix.ui.splash.model.SplashUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -16,6 +17,7 @@ class SplashViewModel @Inject constructor() : BaseViewModel() {
 
     init {
         launch {
+            delay(1500) //dummy delay to show splash screen (request should replace it)
             _uiState.update { it.copy(navigateToHome = true) }
         }
     }
