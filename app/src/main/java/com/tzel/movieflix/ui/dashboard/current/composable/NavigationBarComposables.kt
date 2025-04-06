9d9f1360e58ac5c3eca23805de26e2b3cc1227e9
Tzelalis <ptzelalis@tringlesoft.com>
2025-04-07 01:10:51 +0300

    Update dependencies, fix navigation bar height

--- app/src/main/java/com/tzel/movieflix/ui/dashboard/current/composable/NavigationBarComposables.kt
+++ app/src/main/java/com/tzel/movieflix/ui/dashboard/current/composable/NavigationBarComposables.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,6 +31,7 @@ fun MFNavigationBar(
         items.forEach { navigationItem ->
             NavigationBarItem(
                 selected = currentDestination()?.hierarchy?.any { it.hasRoute(navigationItem.destination::class) } == true,
+                colors = NavigationBarItemDefaults.colors().copy(selectedTextColor = MaterialTheme.colorScheme.surface),
                 icon = {
                     Icon(
                         modifier = Modifier.size(Sizes.Icons.small),
@@ -37,7 +40,10 @@ fun MFNavigationBar(
                     )
                 },
                 label = {
-                    Text(text = navigationItem.label.build())
+                    Text(
+                        text = navigationItem.label.build(),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
                 },
                 onClick = dropUnlessResumed { onItemClick(navigationItem) }
             )
